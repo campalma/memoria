64be0f9e28c4d075e62dacdbf4cab095cfadf90c
Camilo Palma Pradena <cpalmapradena@gmail.com>
2012-08-06 22:31:44 -0400

    Debug require false deply problem

--- eventsvis/settings.py
+++ eventsvis/settings.py
@@ -143,9 +143,9 @@ LOGGING = {
     },
     'handlers': {
         'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
+#            'level': 'ERROR',
+#            'filters': ['require_debug_false'],
+#            'class': 'django.utils.log.AdminEmailHandler'
         }
     },
     'loggers': {
